b87ed51020042eee5607afb8630418587da6c70a
XDean <373216024@qq.com>
2018-01-09 19:05:07 +0800

    TestInterfacePrivateMethod.java

--- src/main/java/xdean/java9/base/TestInterfacePrivateMethod.java
+++ src/main/java/xdean/java9/base/TestInterfacePrivateMethod.java
@@ -1,10 +1,17 @@
 package xdean.java9.base;
 
 public class TestInterfacePrivateMethod {
-  public interface I {
+  public static void main(String[] args) {
+    Impl impl = new Impl();
+//     impl.worker();
+    ((Inter) impl).worker();
+  }
+
+  public interface Inter {
     void func();
 
     private void worker() {
+      System.out.println("TestInterfacePrivateMethod.Inter.worker()");
     }
 
     default void foo() {
@@ -13,6 +20,13 @@ public class TestInterfacePrivateMethod {
 
     default void bar() {
       worker();
+    }
+  }
+
+  public static class Impl implements Inter {
+    @Override
+    public void func() {
+
     }
   }
 }
