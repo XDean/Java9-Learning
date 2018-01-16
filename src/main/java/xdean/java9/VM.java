046b519ecc23caebcd537815af05310106cdbc0a
XDean <373216024@qq.com>
2018-01-16 15:07:15 +0800

    format VM

--- src/main/java/xdean/java9/VM.java
+++ src/main/java/xdean/java9/VM.java
@@ -5,11 +5,11 @@ import java.lang.annotation.Repeatable;
 import xdean.java9.VM.Arg;
 
 @Arg(key = "–illegal-access", value = { "permit", "warn", "debug", "deny" })
-@Arg(key = "--add-exports", value = "$module/$package=$readingmodule")
-@Arg(key = "--add-opens", value = "$module/$package=$reflectingmodule")
-@Arg(key = "--patch-module", value = "$module=$artifact")
-@Arg(key = "--add-modules", value = "$modules")
-@Arg(key = "--add-reads", value = " $module=$targets")
+@Arg(key = "--add-exports",   value = "$module/$package=$readingmodule")
+@Arg(key = "--add-opens",     value = "$module/$package=$reflectingmodule")
+@Arg(key = "--patch-module",  value = "$module=$artifact")
+@Arg(key = "--add-modules",   value = "$modules")
+@Arg(key = "--add-reads",     value = "$module=$targets")
 public class VM {
 
   @Repeatable(Args.class)
