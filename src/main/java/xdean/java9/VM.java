0407048ad55f4f8b3b7056514dd227fac90800d0
XDean <373216024@qq.com>
2018-01-16 11:06:40 +0800

    VM args and TestReflectHolder

---
+++ src/main/java/xdean/java9/VM.java
@@ -0,0 +1,27 @@
+package xdean.java9;
+
+import java.lang.annotation.Repeatable;
+
+import xdean.java9.VM.Arg;
+
+@Arg(key = "–illegal-access", value = { "permit", "warn", "debug", "deny" })
+@Arg(key = "--add-exports", value = "$module/$package=$readingmodule")
+@Arg(key = "--add-opens", value = "$module/$package=$reflectingmodule")
+@Arg(key = "--patch-module", value = "$module=$artifact")
+@Arg(key = "--add-modules", value = "$modules")
+@Arg(key = "--add-reads", value = " $module=$targets")
+public class VM {
+
+  @Repeatable(Args.class)
+  public @interface Arg {
+    String key();
+
+    String[] value() default "";
+
+    String usage() default "";
+  }
+
+  public @interface Args {
+    Arg[] value();
+  }
+}
---
+++ src/main/java/xdean/java9/modules/TestReflect.java
@@ -0,0 +1,6 @@
+package xdean.java9.modules;
+
+public class TestReflect {
+  public static void main(String[] args) {
+  }
+}
